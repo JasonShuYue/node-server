02c6b0ade5ae3e990038816596a9e6922eee73e2
Jason_Shu <shuyue524@gmail.com>
2022-04-05 22:29:24 +0800

    log serser中request相关属性

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import * as http from "http";
+import { IncomingMessage, ServerResponse } from "http";
 
 const server = http.createServer();
 
-server.on("request", (request, response) => {
-  response.end("那边收到啦！");
-  console.log("有人请求了");
+server.on("request", (request: IncomingMessage, response: ServerResponse) => {
+  console.log("request.httpVersion:", request.httpVersion);
+  console.log("request.method:", request.method);
+  console.log("request.url:", request.url);
+  console.log("request.headers:", request.headers);
+
+  response.end("hi!!!");
 });
 
 server.listen("8888");
