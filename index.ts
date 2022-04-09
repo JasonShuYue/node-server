9598657f3d7e24ec92d189bf4fc4ff19cdd6ea47
Jason_Shu <shuyue524@gmail.com>
2022-04-09 14:23:22 +0800

    目标3: 匹配任意文件

--- index.ts
+++ index.ts
@@ -2,35 +2,24 @@ import * as http from "http";
 import { IncomingMessage, ServerResponse } from "http";
 import * as path from "path";
 import * as fs from "fs";
+import * as url from "url";
 
 const server = http.createServer();
 const publicDir = path.resolve(__dirname, "public");
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
-  const { url } = request;
-  switch (url) {
-    case "/index.html":
-      response.setHeader("Content-Type", "text/html; charset=utf-8;");
-      fs.readFile(path.resolve(publicDir, "index.html"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/style.css":
-      response.setHeader("Content-Type", "text/css; charset=utf-8;");
-      fs.readFile(path.resolve(publicDir, "style.css"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/main.js":
-      response.setHeader("Content-Type", "text/javascript; charset=utf-8;");
-      fs.readFile(path.resolve(publicDir, "main.js"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-  }
+  const { url: originUrl } = request;
+  const { pathname } = url.parse(originUrl);
+  const fileName = pathname.substring(1);
+
+  fs.readFile(path.resolve(publicDir, fileName), (error, data) => {
+    if (error) {
+      response.statusCode = 404;
+      response.end("404");
+    } else {
+      response.end(data.toString());
+    }
+  });
 });
 
 server.listen("8888");
